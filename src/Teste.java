111d03b0e4dd136ec464003774ee23e713e104ea
Hugo <hugomgs0203@gmail.com>
2024-10-15 19:36:11 -0300

    Projeto Terminado

--- src/Teste.java
+++ src/Teste.java
@@ -3,33 +3,33 @@ public class Teste {
 
         Veiculo motonull = new Moto(null, null, 0, 0);
 
-        //new Frota().manuntencao(motonull);
+        new Frota().manuntencao(motonull);
 
         Frota frota = new Frota();
 
-        // Veiculo moto = new Moto("Fiat", "CPX", 2023, 0);
+        Veiculo moto = new Moto("Fiat", "CPX", 2023, 0);
 
-        //Veiculo carro = new Carro("Fiat", "Uno", 2010, 0);
+        Veiculo carro = new Carro("Fiat", "Uno", 2010, 0);
 
         frota.adicionarnaFrota(null);
 
-        //Rastreador rastreador = new Rastreador<>();
+        Rastreador rastreador = new Rastreador<>();
 
-        //rastreador.listarEventos(carro); //Colocar erro de listagem de eventos de objetos sem eventos
+        rastreador.listarEventos(carro); //Colocar erro de listagem de eventos de objetos sem eventos
 
-        // EventoDesempenho evento = new EventoDesempenho<Veiculo>(moto, moto.calcularConsumo(), moto.calculareficiencia());
+        EventoDesempenho evento = new EventoDesempenho<Veiculo>(moto, moto.calcularConsumo(), moto.calculareficiencia());
 
-        // rastreador.registrarEvento(moto, evento);
+        rastreador.registrarEvento(moto, evento);
 
-        // rastreador.listarEventos(moto);
+        rastreador.listarEventos(moto);
 
 
-        // new Frota().manuntencao(carro);
+        new Frota().manuntencao(carro);
 
-        // new Frota().CriarViagem("Belém", "Mosqueiro",400, carro);    
+        new Frota().CriarViagem("Belém", "Mosqueiro",400, carro);    
 
-        // new Frota().manuntencao(moto);
+        new Frota().manuntencao(moto);
 
-        // new Frota().CriarViagem("Belém", "Castanhal",200, moto);        
+        new Frota().CriarViagem("Belém", "Castanhal",200, moto);        
     }
 }
